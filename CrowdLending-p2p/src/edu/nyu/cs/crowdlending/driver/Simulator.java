aedc4ad9b34f1f671b41762d8eb67eed7158d14e
Varada Hanumante <varadahanumante@gmail.com>
2020-05-15 12:35:07 -0500

    First commit

---
+++ CrowdLending-p2p/src/edu/nyu/cs/crowdlending/driver/Simulator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.nyu.cs.crowdlending.driver;
+
+/**
+ * @author varada
+ *
+ */
+public class Simulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
