6aa3f627ff739ec819b8b5fe732a8c0f4426201c
Pranali Awasekar <pranali.awasekar04@gmail.com>
2020-05-16 00:02:06 -0400

    Client v2 review

--- CrowdLending-p2p/src/edu/nyu/cs/crowdlending/driver/Client.java
+++ CrowdLending-p2p/src/edu/nyu/cs/crowdlending/driver/Client.java
@@ -5,26 +5,29 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Client {
-	private String name;
-    private InetAddress ip;
+	private String accountNumber;
+    private String ip;
     private int balance;
     private Map<String, Integer> moneyBorrowed;
     private Map<String,Integer> moneyLent;
-    InetAddress serverIp;
+    String serverIp;
     int serverPort;
+    int port;
     
-    ServerSocket sc;
+    ServerSocket serviceSocket;
     
-    public Client(String name, InetAddress ipAddress, int port, int bal, InetAddress serverIp, int serverPort) throws IOException {
-    	sc = new ServerSocket(port);
+    public Client(String accNo, String ipAddress, int port, int bal, InetAddress serverIp, int serverPort) throws IOException {
+    	this.port = port; 
+    	serviceSocket = new ServerSocket(port);
     	this.serverPort = serverPort; 
-    	this.name = name;
+    	this.accountNumber = accNo;
     	ip = ipAddress;
     	balance = bal;
     	moneyBorrowed = Collections.synchronizedMap(new HashMap<>());
@@ -32,49 +35,103 @@ public class Client {
     }
     
     public String getName() {
-    	return name;
+    	return accountNumber;
     }
     
-    public InetAddress getIp() {
+    public String getIp() {
     	return ip;
     }
     
-    public void getEligibleLenders() {
-    	
+    //register client to server
+    public void register() throws UnknownHostException, IOException {
+    	Socket sc = new Socket(serverIp, serverPort);
+    	PrintWriter out = new PrintWriter(sc.getOutputStream(), true);
+        Scanner in = new Scanner(sc.getInputStream());
+        
+        String registerServ = ip+":"+port;
+        //send client's wish to get money
+        out.println("REGISTER "+registerServ);
     }
     
-    //acting as a client
-    public void getMoney(String ipToConnect, int portToConn, int moneyReq) throws IOException {
-    	Socket sc = new Socket(ipToConnect, portToConn);
-    	
+  //ask server for lenders	
+    public String[] getEligibleLendersFromServer(int moneyReq) throws IOException {
+    	Socket sc = new Socket(serverIp, serverPort);
     	PrintWriter out = new PrintWriter(sc.getOutputStream(), true);
         Scanner in = new Scanner(sc.getInputStream());
         
         out.println("BORROW "+moneyReq);
-        String response = in.nextLine();
-        System.out.println(response);
         
+        String[] ipsToConnect = in.nextLine().split(",");
+        
+        return ipsToConnect;
+    }
+    
+    //acting as a client
+    public void borrowMoney(String[] ipsToConnect, int moneyReq) throws IOException {
+    	//connect to server to get list of lenders
+    	Socket sc = new Socket(serverIp, serverPort);
+    	Scanner in = null ;
+    	PrintWriter out = null;
+    	
+        for(String ipsAndPort : ipsToConnect) {
+        	 //get port
+        	String ipAndPort[] = ipsAndPort.split(":");
+        	
+        	 sc = new Socket(ipAndPort[0], Integer.parseInt(ipAndPort[1]));	
+        	out = new PrintWriter(sc.getOutputStream(), true);
+        	in = new Scanner(sc.getInputStream());
+        	 
+        	 out.println("BORROW "+moneyReq);
+        	 String response = in.nextLine(); 
+
+        	 if(response.equals("AWK AVAILABLE")) {
+        		 balance+=moneyReq;
+        		 System.out.println("Money Recieved");
+        		 break;
+        	 }
+        }        
+      //close connections
+        if(in!=null) in.close();
+        if(out!=null) out.close();
+        sc.close();
     }
     
     //acting as a Server
-    public void sendMoney(String clientName) throws IOException {
+    public void lendMoney(String clientName) throws IOException {
     	//accepts the connection from borrower peer
-    	Socket client = sc.accept();
+    	Socket client = serviceSocket.accept();
     	
-    	//<--Might have to edit---->
+    	//<----Might have to edit---->
     	//read the input from client 
     	Scanner input = new Scanner(client.getInputStream());
+    	PrintWriter output = new PrintWriter(client.getOutputStream(), true);
+
     	String msg = input.nextLine();
     	String[] parsed = Message.parseMessage(msg);
     	
     	//update balance
     	int reqMoney =  Integer.parseInt(parsed[parsed.length-1]);
-    	if(reqMoney<balance)
+    	
+    	if(reqMoney<balance) {
+    		output.println("AWK AVAILABLE");
     		balance = balance-reqMoney;
+    	}
+    	else {
+    	 //send message if money not available to send
+    		output.println("AWK NOT AVAILABLE");
+    		
+    		input.close();
+            output.close();
+            client.close();
+            
+    		return;
+    	}
     	
     	//send AWK message
-        PrintWriter output = new PrintWriter(client.getOutputStream(), true);
         output.println("AWK MONEY SENT "+clientName);
+       
+        //update server
+        updateServer();
         
         //close connections
         input.close();
@@ -82,7 +139,14 @@ public class Client {
         client.close();
     }
     
-    public void stopConnections() {
-    	
+    public void updateServer() throws UnknownHostException, IOException {
+    	Socket sc = new Socket(serverIp, serverPort);
+    	PrintWriter out = new PrintWriter(sc.getOutputStream(), true);
+        Scanner in = new Scanner(sc.getInputStream());
+        
+        String registerServ = ip+":"+port;
+        //send client's wish to get money
+        out.println("UPDATE "+balance);
     }
+    
 }
